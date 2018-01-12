258ee007454d74886553afc55d3e4d4149887bf4
Mack Stump <mack.stump@gmail.com>
2018-01-12 15:18:36 -0500

    create context from (device, platform)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,8 +15,35 @@ import static org.lwjgl.opencl.InfoUtil.*;
 import static org.lwjgl.opencl.KHRICD.*;
 import static org.lwjgl.system.MemoryStack.*;
 
+class CLContext {
+    private final long context;
+    private final CLDevice device;
+
+    public CLContext(CLDevice device) {
+        this.device = device;
+
+        try (MemoryStack stack = stackPush()) {
+            PointerBuffer ctxProps = stack.mallocPointer(3);
+            ctxProps.put(0, CL_CONTEXT_PLATFORM)
+                    .put(1, device.getPlatformID())
+                    .put(2, 0);
+            context = clCreateContext(ctxProps, device.getDeviceID(), null, 0, null);
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("Context [0x%x]\n", context));
+        sb.append(String.format("  Device [0x%x]\n", device.getDeviceID()));
+
+        return sb.toString();
+    }
+}
+
 class CLDevice {
     private final long device_id;
+    private final long platform_id;
     private final String DEVICE_NAME;
     private final String DEVICE_VERSION;
     private final String DRIVER_VERSION;
@@ -28,6 +55,7 @@ class CLDevice {
 
     public CLDevice(long device) {
         this.device_id = device;
+        this.platform_id = getDeviceInfoLong(device, CL_DEVICE_PLATFORM);
         this.DEVICE_NAME = getDeviceInfoStringUTF8(device, CL_DEVICE_NAME);
         this.DEVICE_VERSION = getDeviceInfoStringUTF8(device, CL_DEVICE_VERSION);
         this.DRIVER_VERSION = getDeviceInfoStringUTF8(device, CL_DRIVER_VERSION);
@@ -38,12 +66,16 @@ class CLDevice {
         this.DEVICE_TYPE = getDeviceInfoLong(device, CL_DEVICE_TYPE);
     }
 
-    public boolean compileSource(String source) {
-        throw new UnsupportedOperationException();
-    }
-
     public boolean isGPU() {
         return (this.DEVICE_TYPE & CL_DEVICE_TYPE_GPU) != 0;
+    }
+
+    public long getPlatformID() {
+        return this.platform_id;
+    }
+
+    public long getDeviceID() {
+        return this.device_id;
     }
 
     public String toString() {
@@ -161,6 +193,12 @@ public class Main {
         return new String(Files.readAllBytes(file.toPath()));
     }
 
+    public static void demoRunVADD(CLDevice device, String source) {
+        CLContext context = new CLContext(device);
+
+        System.out.println(context);
+    }
+
     public static void main(String args[]) throws IOException {
         String sourcecode = readKernelSource("add.cl");
         // Device to run opencl program on
@@ -187,7 +225,9 @@ public class Main {
         }
 
         if (gpuDevice.isPresent()) {
-            gpuDevice.get().compileSource(sourcecode);
+            demoRunVADD(gpuDevice.get(), sourcecode);
+        } else {
+            System.err.println("No OpenCL compatible GPU found.");
         }
     }
 }
